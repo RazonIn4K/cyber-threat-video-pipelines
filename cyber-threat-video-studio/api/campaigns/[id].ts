552b82debb50e213952238594645910c82a0d508
David Ortiz <89505847+RazonIn4K@users.noreply.github.com>
2025-11-25 18:34:07 -0600

    chore: add doppler start scripts for local api shim

--- cyber-threat-video-studio/api/campaigns/[id].ts
+++
@@ -1,5 +0,0 @@
-import { campaignsApi } from '../../campaigns';
-
-export const getCampaign = campaignsApi.get;
-export const updateCampaign = campaignsApi.update;
-export const deleteCampaign = campaignsApi.remove;
